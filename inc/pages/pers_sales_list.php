be8dccbd66f12e4a8dd87a8148119cd6e05625ec
sebahattin çatal <sebahattin.catal@yandex.com>
2016-10-25 11:49:04 +0300

    edit

--- inc/pages/pers_sales_list.php
+++ inc/pages/pers_sales_list.php
@@ -78,7 +78,6 @@
                     <th>İleri Tarihli</th>
                     <th>İleri T.satış</th>
                     <th>Geçersiz</th>
-                
                     <th>Ürün Adeti</th>
                     <th>Sepet Ort.Adet</th>
                     <th>Sepet Ort.ciro</th>
@@ -99,7 +98,9 @@
                       $dizim[$valuex->stop_case] = $valuex->adet;
                   }
 
-                    $satis = $db->get_row("SELECT sum(fiyat) as ciro, Sum(urun_adeti) as urunAdeti, count(*) as adet  FROM siparisler where  islem_tarihi  between '".$start_data." 00:00:00' AND '".$stop_data." 23:59:59' AND  kaynak_id='".$value->admin_id."' AND siparis_durumu in (7,9)  ".$sql_statu."   ");
+                    /*$satis = $db->get_row("SELECT sum(fiyat) as ciro, Sum(urun_adeti) as urunAdeti, count(*) as adet  FROM siparisler where  islem_tarihi  between '".$start_data." 00:00:00' AND '".$stop_data." 23:59:59' AND  kaynak_id='".$value->admin_id."' AND siparis_durumu in (7,9)  ".$sql_statu."   ");*/
+
+                    $satis = $db->get_row("SELECT sum(fiyat) as ciro, Sum(urun_adeti) as urunAdeti, count(*) as adet  FROM siparisler where  islem_tarihi  between '".$start_data." 00:00:00' AND '".$stop_data." 23:59:59' AND  kaynak_id in (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20) AND siparis_durumu in (7,9)  ".$sql_statu."   ");
 
                       $t1 +=$satis->adet;
                       $t2 +=$dizim[2];
@@ -125,7 +126,7 @@
                               ';
 
                             echo '<td >
-                              <a href="pages.php?ido=pers_source_sales_view&id='.$value->admin_id.'&start='.$start_data.'&stop='.$stop_data.'" class="btn btn-info">Satışları</a>
+                              <a href="pages.php?ido=Pers_sales_view&id='.$value->admin_id.'&start='.$start_data.'&stop='.$stop_data.'" class="btn btn-info">Satışları</a>
                               </td>
                             </tr>';
                          }
